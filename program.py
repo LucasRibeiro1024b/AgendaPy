02776c374799eb068457f3128fc0398b0a190e44
Lucas Ribeiro <lucasribeiro1024b@gmail.com>
2019-12-17 19:05:17 -0300

    Alterado alguns botões e teste com funções

--- program.py
+++ program.py
@@ -1,22 +1,33 @@
 from tkinter import *
+from functools import partial
 
-def bt_click():
-    print("bt click")
-    lb["text"] = "Funcionou!!"
+#funções
+
+def bt_click(but):
+    lb["text"] = "Posso enviar agora, nice man "
 
 def bt_color():
-    janela["background"] = "red"
+    cores = {"blue", "red", "black", "green"}
+    janela["background"] = "black"
+
+#botões e janelas
 
 janela = Tk()
 
-bt = Button(janela, width=10, text="OK", command=bt_click)
-bt.place(x=10, y=100)
+intro = Label(janela, text="Sistema de cadastro de telefone")
+intro.place(x=75, y=10)
 
-bt2 = Button(janela, width=10, text="OK", command=bt_color)
-bt2.place(x=10, y=50)
+butt1 = Button(janela, width=30, text="change text and send text")
+butt1["command"] = partial(bt_click, butt1)
+butt1.place(x=10, y=100)
+
+butt2 = Button(janela, width=10, text="Change color", command=bt_color)
+butt2.place(x=10, y=50)
 
 lb = Label(janela, text="Teste")
 lb.place(x=10, y=150)
 
-janela.geometry("300x600+200+200")
+
+#tamanho da janela e loop do programa
+janela.geometry("350x400+200+200")
 janela.mainloop()
