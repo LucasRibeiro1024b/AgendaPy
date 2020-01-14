a31c3f1c7a68cd7bd54a8ef56369e048d46bab9c
Lucas Ribeiro <lucasribeiro1024b@gmail.com>
2020-01-14 11:00:30 -0300

    Adicionado função alterar()

--- Agenda.py
+++ Agenda.py
@@ -19,9 +19,6 @@ if arquivo not in diretorio:
         email TEXT NOT NULL
     );
     """)
-else:
-    conn = sqlite3.connect("base.db")
-    cursor = conn.cursor()
     
 #functions
 '''
@@ -38,6 +35,9 @@ def del_bd():
 '''
 
 def inserir():
+    conn = sqlite3.connect("base.db")
+    cursor = conn.cursor()
+    
     id_c = int(Id_text.get())
     name_c = nome_text.get()
     numero_c = numero_text.get()
@@ -77,8 +77,13 @@ def inserir():
         nome_text.delete(0, END)
         numero_text.delete(0, END)
         email_text.delete(0, END)
+
+    conn.close()
     
 def remover():
+    conn = sqlite3.connect("base.db")
+    cursor = conn.cursor()
+    
     id_c = Id_text.get()
     cursor.execute("""
     DELETE FROM Contatos WHERE id = ?
@@ -90,33 +95,86 @@ def remover():
     nome_text.delete(0, END)
     numero_text.delete(0, END)
     email_text.delete(0, END)
-    
-def search_id():
+
+    conn.close()
+
+def alterar():
+    conn = sqlite3.connect("base.db")
+    cursor = conn.cursor()
+
     id_c = int(Id_text.get())
-    test = False
+    name_c = nome_text.get()
+    numero_c = numero_text.get()
+    email_c = email_text.get()
 
-    cursor.execute("""
-    SELECT * FROM Contatos;
-    """)
+    all_valid = True
+
+    if len(name_c) <= 40:
+        nome_error["text"] = "OK!"
+    else:
+        all_valid = False
+        nome_error["text"] = "ERROR"
+        nome_text["background"] = "red"
     
-    for linha in cursor.fetchall():
-        if (linha[0] == id_c):
-            test = True
-            nome_text.delete(0, END)
-            nome_text.insert(END, linha[1])
+    if re.match(r'[1-9]{2}[0-9]{9}', numero_c) and len(numero_c) == 11:
+        numero_error["text"] = "OK!"
+    else:
+        all_valid = False
+        numero_error["text"] = "ERROR"
+        numero_text["background"] = "red"
 
-            numero_text.delete(0, END)
-            numero_text.insert(END, linha[2])
-            
-            email_text.delete(0, END)
-            email_text.insert(END, linha[3])
+    if re.match(r'^[\w-]+@[a-z\d]+\.[\w]{3}', email_c):
+        email_error["text"] = "OK!"
+    else:
+        all_valid = False
+        email_error["text"] = "ERROR"
+        email_text["background"] = "red"
 
-    if (not test):
+    if all_valid:
+        cursor.execute("""
+            UPDATE Contatos
+            SET nome = ?, numero = ?, email = ?
+            WHERE id = ?""",
+        (name_c, numero_c, email_c, id_c))
+
+        conn.commit()
+
+        Id_text.delete(0, END)
         nome_text.delete(0, END)
-
         numero_text.delete(0, END)
-            
         email_text.delete(0, END)
+    
+    conn.close()
+
+def search_id():
+    conn = sqlite3.connect("base.db")
+    cursor = conn.cursor()
+    
+    id_c = int(Id_text.get())
+
+    try:
+        cursor.execute("""
+        SELECT * FROM Contatos WHERE id=?;
+        """, (id_c,))
+    
+        for linha in cursor.fetchall():
+            print(linha)
+            if (linha[0] == id_c):
+                test = True
+                nome_text.delete(0, END)
+                nome_text.insert(END, linha[1])
+    
+                numero_text.delete(0, END)
+                numero_text.insert(END, linha[2])
+            
+                email_text.delete(0, END)
+                email_text.insert(END, linha[3])
+                
+    except:
+        nome_text.delete(0, END)
+        numero_text.delete(0, END)
+        email_text.delete(0, END)
+    conn.close()
 
 #Head
     
@@ -134,11 +192,14 @@ search_id.place(x=175, y=40)
 
 #Body
 
-inserir_bt = Button(janela, width=10, text="Inserir/Alterar", command=inserir)
+inserir_bt = Button(janela, width=10, text="Inserir", command=inserir)
 inserir_bt.place(x=10, y=200)
 
 remover_bt = Button(janela, width=10, text="Remover", command=remover)
 remover_bt.place(x=120, y=200)
+
+alterar_bt = Button(janela, width=10, text="Alterar", command=alterar)
+alterar_bt.place(x=230, y=200)
 
 nome_lb = Label(janela, text="Nome")
 nome_lb.place(x=10, y=110)
